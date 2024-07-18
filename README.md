fcedcf14aaa2c5f0abc9289b74b446873725baea
Andreas Bacher <a.bacher@svg.de>
2024-07-18 22:46:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# movie-maven
+
+## Show the world that you are an expert when it comes to movies
+
+    A movie quizzer, either you are a maven of movies or 
+    you have skill issues which the program will point out to you 
+    in some form :-)
+
+    Be a maven, aka
+    - an expert, 
+    - a master, 
+    - ein Experte, 
+    - un maître, 
+    - gosu, 
+    - ...
+
+### Tech Stack
+
+- golang
+- charm bubble tea for tui
+- APIs: omdb
+
+## How to run
+
+    go run . 
+
+or 
+
+    go build
+
+    ./movie-maven
---
+++ mm.go
@@ -0,0 +1,111 @@
+package main
+
+// A simple example that shows how to retrieve a value from a Bubble Tea
+// program after the Bubble Tea has exited.
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var choices = []string{"A ... An actress or actor appeared in all of these movies", "B ... Same director", "C ... Released in the same year"}
+
+type model struct {
+	cursor int
+	choice string
+	list   list.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			// Send the choice on the channel and exit.
+			m.choice = choices[m.cursor]
+			return m, tea.Quit
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := strings.Builder{}
+	s.WriteString("Let me show you three movies that have a thing in common!\n\n")
+	s.WriteString(docStyle.Render(m.list.View())) // TODO show movies as list
+	s.WriteString("What do these movies have in common?\n\n")
+
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return s.String()
+}
+
+func main() {
+	items := []list.Item{
+		item{title: "Alien 1", desc: "scary"},
+		item{title: "My best friends wedding", desc: "what the"},
+		item{title: "Pretty Woman", desc: "oh Julia"},
+	}
+
+	p := tea.NewProgram(model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)})
+
+	// Run returns the model as a tea.Model.
+	m, err := p.Run()
+	if err != nil {
+		fmt.Println("Oh no:", err)
+		os.Exit(1)
+	}
+
+	// Assert the final tea.Model to our local model and print the choice.
+	if m, ok := m.(model); ok && m.choice != "" {
+		fmt.Printf("\n---\nYou chose %s!\n", m.choice)
+	}
+}
